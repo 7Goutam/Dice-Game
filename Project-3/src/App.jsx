5586519b5da35afd9971bb1cda10007b4f030c32
Goutam Jha <jhagoutam804@gmail.com>
2025-04-02 00:00:07 +0530

    1st Commit

---
+++ Project-3/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import GamePlay from "./Components/GamePlay";
+import StartGame from "./Components/StartGame";
+
+
+function App() {
+    const [isGameStarted, setIsGameStarted] = useState(false);
+
+    const toggleGamePlay = () => {
+        setIsGameStarted((prev) => !prev);
+    };
+
+    return (
+       <>
+       {isGameStarted ? <GamePlay /> : <StartGame
+       toggle = {toggleGamePlay} />}
+       </>
+    );
+}
+export default App;
